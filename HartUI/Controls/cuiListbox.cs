182576ef54af18783c3169cf89831a4ed62f539e
rrvvee <rrvvee@DESKTOP-1V01GQG>
2026-07-13 01:56:48 +0200

    cuiListbox: Add `SelectionMode` support & expose OutlineThickness

--- HartUI/Controls/cuiListbox.cs
+++ HartUI/Controls/cuiListbox.cs
@@ -91,7 +91,7 @@ namespace HartUI.Controls
         private int privateOutlineThickness = 1;
 
         [Category("HartUI")]
-        private int OutlineThickness
+        public int OutlineThickness
         {
             get
             {
@@ -123,7 +123,6 @@ namespace HartUI.Controls
                     if (value > (ItemHeight / 2))
                     {
                         privateItemRounding = (ItemHeight / 2) + 1;
-                        ItemRounding = privateItemRounding;
                     }
                     else
                     {
@@ -252,7 +251,6 @@ namespace HartUI.Controls
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            SelectionMode = SelectionMode.One;
             Graphics g = e.Graphics;
             g.SmoothingMode = SmoothingMode.AntiAlias;
             g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
@@ -287,6 +285,9 @@ namespace HartUI.Controls
                 using (Brush hoverTextBrush = new SolidBrush(ItemHoverForegroundColor))
                 using (Brush normalBrush = new SolidBrush(ItemBackgroundColor))
                 using (Brush normalTextBrush = new SolidBrush(ForegroundColor))
+                using (Brush previewBrush = new SolidBrush(Color.FromArgb(
+                    ItemSelectedBackgroundColor.A / 2,
+                    ItemSelectedBackgroundColor)))
                 {
                     // These are references and should not be disposed
                     Brush itemBackgroundBrush, itemForegroundBrush;
@@ -306,7 +307,16 @@ namespace HartUI.Controls
                         int yCenterString = itemRect.Y + (ItemHeight - Font.Height) / 2;
                         string itemText = Items[i].ToString();
 
-                        if (SelectedIndex == i)
+                        bool renderItemAsPreview = privateIsPreviewDragging
+                            && i >= Math.Min(privatePreviewStart, privatePreviewEnd)
+                            && i <= Math.Max(privatePreviewStart, privatePreviewEnd);
+
+                        if (renderItemAsPreview)
+                        {
+                            itemBackgroundBrush = previewBrush;
+                            itemForegroundBrush = normalTextBrush;
+                        }
+                        else if (GetSelected(i))
                         {
                             itemBackgroundBrush = selectedBrush;
                             itemForegroundBrush = selectedTextBrush;
@@ -354,14 +364,69 @@ namespace HartUI.Controls
             }
         }
 
+        private int privateAnchorIndex = -1;
+
+        private bool privateIsPreviewDragging = false;
+        private int privatePreviewStart = -1;
+        private int privatePreviewEnd = -1;
+
         protected override void OnMouseDown(MouseEventArgs e)
         {
-            int temporaryHoveredIndex = IndexFromPoint(e.Location);
-
-            if (temporaryHoveredIndex >= 0 && temporaryHoveredIndex < Items.Count)
+            if (e.Button != MouseButtons.Left)
             {
-                SelectedIndex = temporaryHoveredIndex;
+                base.OnMouseDown(e);
+                return;
+            }
+
+            int index = IndexFromPoint(e.Location);
+
+            if (index >= 0 && index < Items.Count)
+            {
                 SuspendLayout();
+
+                switch (SelectionMode)
+                {
+                    case SelectionMode.None:
+                        break;
+
+                    case SelectionMode.One:
+                        SelectedIndex = index;
+                        privateAnchorIndex = index;
+                        break;
+
+                    case SelectionMode.MultiSimple:
+                        SetSelected(index, !GetSelected(index));
+                        privateAnchorIndex = index;
+                        break;
+
+                    case SelectionMode.MultiExtended:
+                        bool ctrl = (ModifierKeys & Keys.Control) == Keys.Control;
+                        bool shift = (ModifierKeys & Keys.Shift) == Keys.Shift;
+
+                        if (shift && ctrl && privateAnchorIndex >= 0)
+                        {
+                            ExtendRange(privateAnchorIndex, index);
+                        }
+                        else if (shift && privateAnchorIndex >= 0)
+                        {
+                            SelectRange(privateAnchorIndex, index);
+                        }
+                        else if (ctrl)
+                        {
+                            SetSelected(index, !GetSelected(index));
+                            privateAnchorIndex = index;
+                        }
+                        else
+                        {
+                            privateIsPreviewDragging = true;
+                            privatePreviewStart = index;
+                            privatePreviewEnd = index;
+                            privateAnchorIndex = index;
+                            Capture = true;
+                        }
+                        break;
+                }
+
                 Invalidate();
                 ResumeLayout(true);
             }
@@ -369,12 +434,68 @@ namespace HartUI.Controls
             base.OnMouseDown(e);
         }
 
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            if (privateIsPreviewDragging)
+            {
+                SuspendLayout();
+
+                SelectRange(privatePreviewStart, privatePreviewEnd);
+                privateAnchorIndex = privatePreviewStart;
+
+                privateIsPreviewDragging = false;
+                privatePreviewStart = -1;
+                privatePreviewEnd = -1;
+                Capture = false;
+
+                Invalidate();
+                ResumeLayout(true);
+            }
+
+            base.OnMouseUp(e);
+        }
+
+        private void SelectRange(int start, int end)
+        {
+            ClearSelected();
+
+            int lo = Math.Max(0, Math.Min(start, end));
+            int hi = Math.Min(Items.Count - 1, Math.Max(start, end));
+
+            for (int i = lo; i <= hi; i++)
+            {
+                SetSelected(i, true);
+            }
+        }
+
+        private void ExtendRange(int start, int end)
+        {
+            int lo = Math.Max(0, Math.Min(start, end));
+            int hi = Math.Min(Items.Count - 1, Math.Max(start, end));
+
+            for (int i = lo; i <= hi; i++)
+            {
+                SetSelected(i, true);
+            }
+        }
+
         protected override void OnMouseMove(MouseEventArgs e)
         {
             privateHoveredIndex = IndexFromPoint(e.Location);
+
             if (e.Button == MouseButtons.Left)
             {
-                OnMouseDown(e);
+                if (SelectionMode == SelectionMode.One)
+                {
+                    OnMouseDown(e);
+                }
+                else if (SelectionMode == SelectionMode.MultiExtended && privateIsPreviewDragging)
+                {
+                    if (privateHoveredIndex >= 0 && privateHoveredIndex < Items.Count)
+                    {
+                        privatePreviewEnd = privateHoveredIndex;
+                    }
+                }
             }
 
             Invalidate();
@@ -398,8 +519,28 @@ namespace HartUI.Controls
         private const int WM_VSCROLL = 0x115;
         private const int WM_MSCROLL = 0x20A;
 
+        // Mouse button (for multiselect support)
+        private const int WM_LBUTTONDOWN = 0x0201;
+        private const int WM_LBUTTONDBLCLK = 0x0203;
+
         protected override void WndProc(ref Message m)
         {
+            if ((m.Msg == WM_LBUTTONDOWN || m.Msg == WM_LBUTTONDBLCLK)
+                && (SelectionMode == SelectionMode.MultiSimple || SelectionMode == SelectionMode.MultiExtended))
+            {
+                if (!Focused)
+                {
+                    Focus();
+                }
+
+                long lParam = m.LParam.ToInt64();
+                int x = (short)(lParam & 0xFFFF);
+                int y = (short)((lParam >> 16) & 0xFFFF);
+
+                OnMouseDown(new MouseEventArgs(MouseButtons.Left, 1, x, y, 0));
+                return;
+            }
+
             base.WndProc(ref m);
 
             if (m.Msg == WM_VSCROLL || m.Msg == WM_MSCROLL)
